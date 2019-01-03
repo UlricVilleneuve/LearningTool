68fb467cd722264675c099bdef7556941ad6b26f
Ulric Villeneuve <ulric.villeneuve@hotmail.com>
2019-01-02 23:26:02 -0500

    Created project

---
+++ LearningTool/src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        /* TODO
+        Load the main UI.
+        Display a LoadModule button.
+
+        When the loads a module, create a new tab and display it in the tab.
+        Find a way to have the module return a view and then display this view inside the tab.
+        Or maybe give the module the "tab", and make the module display himself in the tab.
+
+        */
+    }
+}
